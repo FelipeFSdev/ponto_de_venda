ca13bb9b3583de0ebff999db32cea32e7b4af8d2
Felipe <felipe.a.melo@hotmail.com>
2023-11-25 07:42:11 -0300

    Rotas sem callback de requisição

--- src/rotas.js
+++ src/rotas.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const rotas = express();
 
+rotas.use(express.json());
+
+rotas.get("/categoria",);
+rotas.post("/usuario",);
+rotas.post("/login",);
+rotas.get("/usuario",);
+rotas.put("/usuario",);
+
 module.exports = rotas;
