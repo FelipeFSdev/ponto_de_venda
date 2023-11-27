2f8979fc2fd31eb4dcd70e5bfd31b26d087bef98
Rai Saraiva <rai.saraiva70@gmail.com>
2023-11-27 18:48:13 -0300

    Implantacao endpoint loginUsuario

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,5 +1,7 @@
 const knex = require('../conexao/conexaopg');
 const bcrypt = require('bcrypt');
+const senhaJwt = require('../senhaJwt');
+const jwt = require('jsonwebtoken');
 
 const cadastrarUsuario = async (req, res) => {
     const { nome, email, senha } = req.body;
@@ -24,6 +26,38 @@ const cadastrarUsuario = async (req, res) => {
     }
 };
 
+const loginUsuario = async (req, res,) => {
+    const {email, senha} = req.body
+
+    try {
+        
+        const {rows, rowCount} = await pool.query('select * from usuarios where email = $1', [email])
+
+        if (rowCount === 0) {
+            return res.status(400).json({mensagem: 'Email ou senha inválida'})
+        }
+        
+        const {senha: senhaUsuario, ...usuario} = rows[0]
+
+        const senhaCorreta = await bcrypt.compare(senha, senhaUsuario)
+
+        if (!senhaCorreta) {
+            return res.status(400).json({mensagem: 'Email ou senha inválida'})
+        }
+
+        const token = jwt.sign({id: usuario.id}, senhaJwt, {expiresIn: '8h'})
+
+        return res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro interno do servidor'})
+    }
+}
+
 module.exports = {
     cadastrarUsuario,
+    loginUsuario
 }
