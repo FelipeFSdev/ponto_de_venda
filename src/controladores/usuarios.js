ef900f44488fdd6d4e992742efda5dc07f152a05
Felipe Araújo <141762588+FelipeFSdev@users.noreply.github.com>
2023-11-30 07:42:06 -0300

    Update usuarios.js

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -116,7 +116,8 @@ const detalharUsuario = async (req, res) => {
 
 module.exports = {
     cadastrarUsuario,
+    editarUsuario,
     loginUsuario,
     detalharUsuario,
-    editarUsuario
+    
 }
